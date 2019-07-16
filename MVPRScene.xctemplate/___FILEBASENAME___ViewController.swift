ff264cb1407b7c13598581b2af0b3a7d63fdeab9
Sergio Garcia <sgarcia@irontec.com>
2019-07-16 11:32:49 +0200

    ViewController template updated

--- MVPRScene.xctemplate/___FILEBASENAME___ViewController.swift
+++ MVPRScene.xctemplate/___FILEBASENAME___ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___: UIViewController, ___VARIABLE_sceneName:identifier___PresenterView {
-  
-    private(set) lazy var presenter = ___VARIABLE_sceneName:identifier___Presenter(
-        view: self,
-        router: ___VARIABLE_sceneName:identifier___Router(routing: self)
-    )
-  
+
+    private(set) lazy var presenter: ___VARIABLE_sceneName:identifier___Presenter = {
+        let router = ___VARIABLE_sceneName:identifier___Router(routing: self)
+        return ___VARIABLE_sceneName:identifier___Presenter(view: self,
+                              router: router)
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.presenter.viewDidLoad()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.presenter.viewWillAppear()
@@ -29,17 +30,17 @@ class ___FILEBASENAMEASIDENTIFIER___: UIViewController, ___VARIABLE_sceneName:id
         super.viewDidAppear(animated)
         self.presenter.viewDidAppear()
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.presenter.viewWillDisappear()
     }
-    
+
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         self.presenter.viewDidDisappear()
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         self.presenter.prepare(for: segue, sender: sender)
     }
